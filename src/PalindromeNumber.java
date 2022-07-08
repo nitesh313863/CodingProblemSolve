d2c7a764cf1442eea6c66df918e383e9b64b463f
Nitesh Yadav <ny313863@gmail.com>
2022-07-08 16:17:45 +0530

    PalindromeNumber

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class PalindromeLogicClass
+{
+    public boolean palindromeNumber(int n)
+    {
+        int p=0;
+        int n1=n;
+       while(n!=0)
+       {
+           p=p*10+n%10;
+           n=n/10;
+       }
+       if(p==n1)
+       {
+           return true;
+       }
+       return false;
+    }
+
+}
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n= sc.nextInt();
+        PalindromeLogicClass ob=new PalindromeLogicClass();
+        System.out.println(ob.palindromeNumber(n));
+
+    }
+}
