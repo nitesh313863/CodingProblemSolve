e47be07d744a62e845a76fdefc8b7da20d84a874
Nitesh Yadav <ny313863@gmail.com>
2022-07-19 22:35:58 +0530

    ALlThe26AlphabetCharacterInSameStringIsCalledParagramString

---
+++ src/PanagramStringApp.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Paragram
+{
+    public boolean paraGram(String s)
+    {
+        if(s.length()<26)
+        {
+            return false;
+        }
+        else
+        {
+            for(char ch='a';ch<='z';ch++)
+            {
+                if(s.indexOf(ch)<0)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+public class PanagramStringApp {
+    public static void main(String[] args) {
+        String s;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String ");
+        s=sc.nextLine();
+        Paragram ob=new Paragram();
+        System.out.println(ob.paraGram(s));
+
+    }
+}
